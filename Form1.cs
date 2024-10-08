c250de1fd9a81ea615c405b530867f37d9b6b3b0
miguel.oliveira <miguel.ao1086@gmail.com>
2024-10-08 12:01:31 -0300

    Resolução de bug crashando a aplicação quando a janela era redimensionada

--- Form1.cs
+++ Form1.cs
@@ -7,21 +7,35 @@ namespace DotVideo
     {
         private FilterInfoCollection _fic;
         private VideoCaptureDevice? _vcd;
+
         public Form1()
         {
             InitializeComponent();
             _fic = new(FilterCategory.VideoInputDevice);
         }
-
         private void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
             // Copia a imagem do buffer da camera
             using Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
             bitmap.RotateFlip(RotateFlipType.Rotate90FlipNone);
-            using Graphics g = panel1.CreateGraphics();
-            g.DrawImage(bitmap, 0, 0);
-        }
 
+            // Obter dimensões para recortar a imagem
+            int yOffset = (bitmap.Height - bitmap.Width) / 2;
+            Rectangle srcRectangle = new(
+                new Point(0, yOffset),
+                new Size(bitmap.Width, bitmap.Width));
+
+            // Obter dimensões do panel para conter a imagem
+            int w = Math.Min(panel1.Width, panel1.Height); // Obter a menor dimensão, imagem quadrada
+
+            int x = (panel1.Width - w) / 2;
+            int y = (panel1.Height - w) / 2;
+
+            Rectangle destRectangle = new(x, y, w, w);
+
+            using Graphics g = panel1.CreateGraphics();
+            g.DrawImage(bitmap, destRectangle, srcRectangle, GraphicsUnit.Pixel);
+        }
         private void UpdateVideoDevices()
         {
             _fic = new(FilterCategory.VideoInputDevice);
@@ -32,7 +46,6 @@ namespace DotVideo
                 comboBox1.Items.Add(dev.Name);
             }
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             UpdateVideoDevices();
@@ -42,7 +55,6 @@ namespace DotVideo
                 true);
             UpdateStyles();
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             if (comboBox1.SelectedIndex < 0)
@@ -50,12 +62,10 @@ namespace DotVideo
                 return;
             }
             _vcd = new(_fic[comboBox1.SelectedIndex].MonikerString);
-            var capabilities = _vcd.VideoCapabilities[1];
-            _vcd.VideoResolution = capabilities;
+            _vcd.VideoResolution = _vcd.VideoCapabilities[0];
             _vcd.NewFrame += FinalFrame_NewFrame;
             _vcd.Start();
         }
-
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (_vcd != null)
